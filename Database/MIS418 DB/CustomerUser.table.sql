3d2ca35eba0487f439bc39d21b58daa5ba993fd6
Stephen Arychuk <sarychuk@ualberta.ca>
2015-11-25 21:06:31 -0700

    Added MyAccounts
    Also deleted useless web pages

---
+++ Database/MIS418 DB/CustomerUser.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Customer_User]
+(
+	UserID uniqueidentifier NOT NULL,
+	CustomerID INT NOT NULL
+
+	CONSTRAINT pk_UserID_CustomerID PRIMARY KEY (UserID,CustomerID),
+	CONSTRAINT fk_UserID FOREIGN KEY (UserID) REFERENCES [dbo].[aspnet_Users](UserId),
+	CONSTRAINT fk_CustomerID FOREIGN KEY (CustomerID) REFERENCES [dbo].[Customer](CustomerID)
+)
