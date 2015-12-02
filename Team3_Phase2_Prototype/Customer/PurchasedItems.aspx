f068bf999dde8448da9c55880c9e5629135ea7a3
Timotei Albu <timotei@ualberta.ca>
2015-12-01 22:04:52 -0700

    Added ability to go to reviews from purchased items

--- Team3_Phase2_Prototype/Customer/PurchasedItems.aspx
+++ Team3_Phase2_Prototype/Customer/PurchasedItems.aspx
@@ -41,6 +41,14 @@
                     <FooterStyle HorizontalAlign="Center" VerticalAlign="Middle" />
                     <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
                 </asp:TemplateField>
+                <asp:TemplateField HeaderText="Reviews">
+                    <ItemTemplate>
+                        <asp:Button ID="btnReview" runat="server" CausesValidation="false" CommandName="GoToReviews"
+                            Text="Go to reviews..." CommandArgument="<%# Container.DataItemIndex %>" />
+                    </ItemTemplate>
+                    <FooterStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                    <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                </asp:TemplateField>
             </Columns>
             <EditRowStyle BackColor="#2461BF" />
             <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
--- Team3_Phase2_Prototype/Customer/PurchasedItems.aspx.vb
+++ Team3_Phase2_Prototype/Customer/PurchasedItems.aspx.vb
@@ -8,11 +8,26 @@ Partial Class Customer_OnSale
 
     Protected Sub GridView1_RowCommand(sender As Object, e As System.Web.UI.WebControls.GridViewCommandEventArgs) Handles GridView1.RowCommand
         Dim grid As GridView = CType(sender, GridView)
+        Dim index As Integer = CType(e.CommandArgument, Integer)
+        Dim selectedGridRow As GridViewRow = grid.Rows(index)
         If e.CommandName = "GoToCourse" Then
-            Dim index As Integer = CType(e.CommandArgument, Integer)
-            Dim selectedGridRow As GridViewRow = grid.Rows(index)
             Customer.CurrentCourse = selectedGridRow.Cells(1).Text
             Response.Redirect("CourseContent.aspx")
+        ElseIf e.CommandName = "GoToReviews" Then
+            Customer.ReviewItemID = CType(selectedGridRow.Cells(0).Text, Integer)
+            Customer.ReviewItemName = selectedGridRow.Cells(1).Text
+            Response.Redirect("~/Customer/ViewReview.aspx")
+        End If
+    End Sub
+
+    Protected Sub GridView2_RowCommand(sender As Object, e As System.Web.UI.WebControls.GridViewCommandEventArgs) Handles GridView2.RowCommand
+        Dim grid As GridView = CType(sender, GridView)
+        Dim index As Integer = CType(e.CommandArgument, Integer)
+        Dim selectedGridRow As GridViewRow = grid.Rows(index)
+        If e.CommandName = "GoToReviews" Then
+            Customer.ReviewItemID = CType(selectedGridRow.Cells(0).Text, Integer)
+            Customer.ReviewItemName = selectedGridRow.Cells(1).Text
+            Response.Redirect("~/Customer/ViewReview.aspx")
         End If
     End Sub
 End Class
